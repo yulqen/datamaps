80d26e1a5be3f32fe04154d6edc0de2744c62994
Matthew Lemon <lemon@matthewlemon.com>
2019-10-02 13:51:17 +0100

    cleanup and some refactoring

--- datamaps/tests/test_cli.py
+++ datamaps/tests/test_cli.py
@@ -2,10 +2,9 @@ import os
 import shutil
 from pathlib import Path
 
-import pytest
 from click.testing import CliRunner
 
-from datamaps.main import cli, _import
+from datamaps.main import _import
 
 
 def test_no_expected_sheet_in_batch_import_to_master(mock_config, resource_dir):
@@ -19,16 +18,20 @@ def test_no_expected_sheet_in_batch_import_to_master(mock_config, resource_dir):
     """
     runner = CliRunner()
     mock_config.initialise()
-    for fl in os.listdir(resource_dir):
-        if os.path.isfile(os.path.join(resource_dir, fl)):
-            shutil.copy(
-                Path.cwd() / "tests" / "resources" / fl,
-                (Path(mock_config.PLATFORM_DOCS_DIR) / "input"),
-                )
+    _copy_resources_to_input(mock_config, resource_dir)
     result = runner.invoke(_import, ["templates", "-m"])
     assert "Expected Sheet Missing: sheet Introduction in test_template.xlsm is expected from" \
            " datamap.csv. Not processing that file until fixed." in result.output
     # assert "Imported data from input/dft1_temp.xlsm to output/master.xlsx." in result.output
     # assert "Finished." in result.output
+
+
+def _copy_resources_to_input(mock_config, resource_dir):
+    for fl in os.listdir(resource_dir):
+        if os.path.isfile(os.path.join(resource_dir, fl)):
+            shutil.copy(
+                Path.cwd() / "tests" / "resources" / fl,
+                (Path(mock_config.PLATFORM_DOCS_DIR) / "input"),
+            )
 
 
