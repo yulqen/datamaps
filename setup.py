d030d2d2d5be1ee2ec8b4a1939da9ea4cf4506d5
Matthew Lemon <lemon@matthewlemon.com>
2020-03-14 16:46:56 +0000

    fix for error in last version to match bcompiler-engine

--- setup.py
+++ setup.py
@@ -36,6 +36,6 @@ setup(
     install_requires=[
         'click',
         'python-dateutil',
-        'bcompiler-engine>=1.0.2'
+        'bcompiler-engine>=1.0.3'
     ],
     test_suite='datamaps.tests')
