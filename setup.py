6c3a96003e413b6a78f312b556727412a7adce3e
Matthew Lemon <matt@matthewlemon.com>
2022-06-21 22:55:39 +0100

    housekeeping - adding some pytest config and other stuff

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
         "Intended Audience :: Other Audience",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Text Processing",
     ],
     keywords='data console commandline excel',
