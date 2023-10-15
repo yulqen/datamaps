22eced6ca50490bc3b7c8bcb5f17f743774c6b24
Matthew Lemon <y@yulqen.org>
2023-10-15 15:40:36 +0100

    Fix test and update pyproject file

--- datamaps/tests/test_cleanser.py
+++ datamaps/tests/test_cleanser.py
@@ -5,25 +5,25 @@ from ..process.cleansers import Cleanser
 def test_cleaning_dot_date():
     ds = "25.1.72"
     ds_double = "25.01.72"
-    four_year = "25.01.1972"
+    four_year = "25.01.2072"
     c = Cleanser(ds)
     c_double = Cleanser(ds_double)
     c_four = Cleanser(four_year)
-    assert c.clean() == datetime.date(1972, 1, 25)
-    assert c_double.clean() == datetime.date(1972, 1, 25)
-    assert c_four.clean() == datetime.date(1972, 1, 25)
+    assert c.clean() == datetime.date(2072, 1, 25)
+    assert c_double.clean() == datetime.date(2072, 1, 25)
+    assert c_four.clean() == datetime.date(2072, 1, 25)
 
 
 def test_cleaning_slash_date():
     ds = "25/1/72"
     ds_double = "25/01/72"
-    four_year = "25/01/1972"
+    four_year = "25/01/2072"
     c = Cleanser(ds)
     c_double = Cleanser(ds_double)
     c_four = Cleanser(four_year)
-    assert c.clean() == datetime.date(1972, 1, 25)
-    assert c_double.clean() == datetime.date(1972, 1, 25)
-    assert c_four.clean() == datetime.date(1972, 1, 25)
+    assert c.clean() == datetime.date(2072, 1, 25)
+    assert c_double.clean() == datetime.date(2072, 1, 25)
+    assert c_four.clean() == datetime.date(2072, 1, 25)
 
 
 def test_em_dash_key():
